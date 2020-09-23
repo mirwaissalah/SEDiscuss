e1fd2236b2ed357abafb6e27ce194424ace618aa
mirwaissalah <71780078+mirwaissalah@users.noreply.github.com>
2020-09-23 20:36:20 +0430

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,11 +1,18 @@
+//Second edit
 public class Discussion{
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         float sum = 0;
+        float num = 0;
         for(int i=1; i<=3; i++){
             System.out.print("Enter the number " + "(" + i + "): ");
             num = input.nextFloat();
             sum = sum + num;
         }
         System.out.println("The average value is " + average(sum));
+    }
+    public static float average(float y){
+        float avg = y / 3;
+        return  avg;
     }
 }
