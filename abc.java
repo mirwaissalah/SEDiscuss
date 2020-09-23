5dc2a9f27851279bc2dfeb28285f68d5fead857e
mirwaissalah <71780078+mirwaissalah@users.noreply.github.com>
2020-09-23 20:40:52 +0430

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,11 +1,9 @@
-//Second edit
-// the previous one was first edit i got mistaken
+//third edit
 import java.util.Scanner;
 public class Discussion{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         float sum = 0;
-        float num = 0;
         for(int i=1; i<=3; i++){
             System.out.print("Enter the number " + "(" + i + "): ");
             num = input.nextFloat();
